353832d80a570d89f359d622d22bb7c8d0c1dd19
Ryan Brill <ryguy123@berkeley.edu>
2022-06-03 01:34:05 -0700

    bayes park fx stan!

--- bayes_parkFx/sim1-4.R
+++ bayes_parkFx/sim1-4.R
@@ -4,7 +4,7 @@ SEED = 4 #FIXME
 OUTPUT_FILE = paste0("sim1-",SEED)
 
 
-source("sim1_main")
+source("sim1_main.R")
 fit1 = fit_model1()
 saveRDS(fit1, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
 saveRDS(y, file = paste0(output_folder, "y_", OUTPUT_FILE, ".rds"))
